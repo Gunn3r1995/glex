0a7ff6a32a1c7aac1290c94ad2febc14020d2204
Shane Smith <Gunn3r1995@ln-mowifi-05-10.1.53.157.net.bton.ac.uk>
2016-01-08 14:28:32 +0000

    Travis ci Test 2

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -169,11 +169,11 @@ void GameWorld::Camera_Control(char key) {
   if (key == '>') {        //Camera look right
         Camera_Horizontal -= 0.5f * Mouse_Sensitivity;
  }
-  if (key == '+') {        //Jump up by 2 blocks/ 0.1, Would like to improve it to a actually jump mechanic
-        Camera_Position += 0.1f;
+  if (key == '+') {        //Jump up/ 0.1, Would like to improve it to a actually jump mechanic
+        Camera_Position += 0.1f * Player_Speed;
  }
   if (key == '-') {       // Drop down by 2 blocks/ -0.1
-        Camera_Position -= 0.1f;
+        Camera_Position -= 0.1f * Player_Speed;
  }
 }
 
