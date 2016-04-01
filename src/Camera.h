4874d034a24b7872e3b3529e79e2d0fb759c935a
Shane Smith <Gunn3r1995@Unknown-00-90-4c-0d-f4-3e.home>
2016-04-01 21:28:31 +0100

    adding camera variables to camera class

--- src/Camera.h
+++ src/Camera.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include "common.h"
 #include "GameAsset.h"
+#include <GL/gl.h>
 
 class Camera{
 
@@ -13,6 +14,20 @@ public:
   Camera();
   glm::mat4 CameraUpdate(Movement movement, int Mouse_X, int Mouse_Y);
 private:
+
+        glm::vec3 Camera_Position;
+
+        glm::vec3 direction;
+	glm::vec3 right;
+	glm::vec3 up;
+
+        GLfloat Camera_Horizontal;
+        GLfloat Camera_Vertical;
+
+        GLfloat Camera_Delta_X;
+	GLfloat Camera_Delta_Y;
+
+        GLfloat Camera_Sensitivity;
 
 };
 
--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -281,51 +281,6 @@ void GameWorld::Camera_Control(char key) {
 /// Sends the camera positions and movements to the translate shader
 //////////////////////////////////////////////////////////////////////////////////////////
 void GameWorld::Draw() {
-        //////////////////////////////////////////////////////////////////////////////////////////
-        /// Camera Direction
-        ///  Calculates the distance each camera movement changes the camera direction
-        //////////////////////////////////////////////////////////////////////////////////////////
-        glm::vec3 direction(
-		cos(Camera_Vertical) * sin(Camera_Horizontal),
-		sin(Camera_Vertical),
-		cos(Camera_Vertical) * cos(Camera_Horizontal)
-	);
-
-	Movement_Z = direction;
-
-	Movement_X = glm::vec3(
-		sin(Camera_Horizontal - 3.14f/2.0f),
-		0,
-		cos(Camera_Horizontal - 3.14f/2.0f)
-	);
-
-	glm::vec3 vup = glm::cross(Movement_X, direction);
-        //////////////////////////////////////////////////////////////////////////////////////////
-        /// Projection matrix. : degree = 45, Field of View = 4:3 ratio, display = 0.1 unit <-> 1000 units
-        //////////////////////////////////////////////////////////////////////////////////////////
-	glm::mat4 Camera_Projection = glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 1000.0f);
-        // Where the Camera Looks at
-        //////////////////////////////////////////////////////////////////////////////////////////
-        ///  Camera view matrix.
-        ///  changes where the camera position looks up to use the camera position
-        ///  changes the direction you look at
-        ///  Makes the world the correct orientation
-        //////////////////////////////////////////////////////////////////////////////////////////
-	glm::mat4 Camera_View = glm::lookAt(
-		Camera_Position,
-		Camera_Position + direction,
-		vup
-	);
-	glm::mat4 Camera_Model(1.0f);
-
-        //////////////////////////////////////////////////////////////////////////////////////////
-        /// Send data to Uniform Variable.
-        /// Sends the data to the translate.vs shader
-        //////////////////////////////////////////////////////////////////////////////////////////
-	glUniformMatrix4fv(0, 1, GL_FALSE, &Camera_Projection[0][0]);
-	glUniformMatrix4fv(1, 1, GL_FALSE, &Camera_View[0][0]);
-	glUniformMatrix4fv(2, 1, GL_FALSE, &Camera_Model[0][0]);
-
         asset_manager->Draw();
 }
 
